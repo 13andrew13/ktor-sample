b7690a647197d896e0a3a3eddd1332cc0f4c70a3
13andrew13 <andrii.polischuk@uptech.team>
2023-01-17 23:31:43 +0200

    Init project

---
+++ src/main/kotlin/com/andrew/Application.kt
@@ -0,0 +1,17 @@
+package com.andrew
+
+import io.ktor.server.application.*
+import io.ktor.server.engine.*
+import io.ktor.server.netty.*
+import com.andrew.plugins.*
+
+fun main() {
+    embeddedServer(Netty, port = 8080, host = "0.0.0.0", module = Application::module)
+        .start(wait = true)
+}
+
+fun Application.module() {
+    configureTemplating()
+    configureHTTP()
+    configureRouting()
+}
